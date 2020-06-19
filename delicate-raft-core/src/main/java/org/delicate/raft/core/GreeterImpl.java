d5ce9043bf3dfa5226f25fb7d0eb9e0cd5f4fa96
tdytaylor <949722633@qq.com>
2020-06-19 17:38:23 +0800

    feat(add protbuf file): add grpc protbuf file

---
+++ delicate-raft-core/src/main/java/org/delicate/raft/core/GreeterImpl.java
@@ -0,0 +1,9 @@
+package org.delicate.raft.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GreeterImpl {
+
+  private static final Logger LOG = LoggerFactory.getLogger(GreeterImpl.class);
+}
